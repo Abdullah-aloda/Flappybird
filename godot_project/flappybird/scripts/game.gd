0e106be2aa6a80228674820bb1d82ff047a3877d
Abdullah-aloda <abdullah.aloda.work@gmail.com>
2025-10-21 20:05:02 -0400

    Did flappybird Did something better
    I recently spoke with some contributors from Moonshot and they let me know I should’ve been committing my progress regularly. I didn’t realize that at first, but they were super understanding and told me I could commit everything now. Before I even got to that point, I had to learn Godot from scratch. This was my first time actually coding, so it took a while to get comfortable. I signed up for a YouTuber’s Godot help course and started by building Flappy Bird. That part didn’t take too long, maybe around 20 ish hours, but then I had a way better idea. I wanted to connect the concept to outer space and make something unique. The game I’m building now puts you in the role of an astronaut in space, destroying enemy ships to earn coins. You’ll use those coins to upgrade weapons, health, and other features I plan to add. I’ve spent my time learning how to bring this idea to life, not just copying tutorials but actually understanding the code and building it myself. So far I’ve created the outer space scene, added the player spaceship and enemy ships, and implemented laser shooting. The lasers work FINALLY! they hit enemies and eliminate them. Enemies can also eliminate you if they collide with you or reach your base. I’ve also designed and coded the physics system through tons of trial and error. It took a veryyy longgg time, but I wanted to truly learn how it works instead of just following along.

---
+++ godot_project/flappybird/scripts/game.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var laser_container = $LaserContainer
+
+var player = null
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	assert(player!=null) # didnt add next part
+	player.laser_shot.connect(_on_player_laser_shot)
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func _on_player_laser_shot(laser_scene, location):
+	var laser = laser_scene.instantiate()
+	laser.global_position = location
+	laser_container.add_child(laser)
---
+++ godot_project/flappybird/scripts/player.gd
@@ -0,0 +1,33 @@
+class_name Player extends CharacterBody2D
+
+signal laser_shot(laser_scene, location)
+
+@export var speed = 300
+
+@onready var muzzle = $Muzzle
+
+var laser_scene = preload("res://scenes/laser.tscn")
+
+var shoot_cd := false
+
+func _process(delta):
+	if Input.is_action_pressed("shoot"):
+		if !shoot_cd:
+			shoot_cd = true
+			shoot()
+			await get_tree().create_timer(.25).timeout
+			shoot_cd = false
+
+func _physics_process(delta):
+	var direction = Vector2(Input.get_axis("move_left",
+"move_right"), Input.get_axis("move_up", "move_down"))
+	print(direction)
+	velocity = direction * speed
+	move_and_slide()
+	
+
+func shoot():
+	laser_shot.emit(laser_scene, muzzle.global_position)
+	
+func die():
+	queue_free()
